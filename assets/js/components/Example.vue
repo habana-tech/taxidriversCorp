140b37e8c7f232b1be7be5b8b7b033c05c3c0e3a
lexz451 <lexz.zaldivar@gmail.com>
2019-09-05 21:05:46 -0400

    Prueba

--- assets/js/components/Example.vue
+++ assets/js/components/Example.vue
@@ -1,11 +1,17 @@
     <template>
        <div>
            <p>This is an example of a new components in VueJs</p>
+           <span>{{ year }}</span>
        </div>
     </template>
     <script>
        export default {
-           name: "example"
+           name: "example",
+           data() {
+               return {
+                   year: new Date().getFullYear()
+               }
+           }
        }
     </script>
     <style scoped>
