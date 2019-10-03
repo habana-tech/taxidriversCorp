f1fe876eb72500a5f32ed51a4b8cb265e9b33557
Josue Miguel <josmiguel92@gmail.com>
2019-10-03 18:22:19 -0400

    multi domain init config

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -8,12 +8,47 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontendController extends AbstractController
 {
     /**
-     * @Route("/", name="frontend")
+     * @Route("/",  
+	*		name="frontend",
+     * 		host="{domain}",
+     *     	defaults={"domain"="%taxidrivers_domain%"},
+     *     	requirements={"domain"="%taxidrivers_domain%"}
+     * )
      */
     public function index()
     {
         return $this->render('frontend/index.html.twig', [
             'controller_name' => 'FrontendController',
+        ]);
+    }
+
+    /**
+     * @Route("/",  
+	*		name="vinales_frontend",
+     * 		host="{domain}",
+     *     	defaults={"domain"="%vinales_domain%"},
+     *     	requirements={"domain"="%vinales_domain%"}
+     * )
+     */
+    public function indexVinales()
+    {
+        return $this->render('frontend/index.html.twig', [
+            'controller_name' => 'indexVinales',
+        ]);
+    }
+
+    /**
+     * @Route("/",  
+	*		name="dash_frontend",
+     * 		host="{domain}",
+     *     	defaults={"domain"="%dash_domain%"},
+     *     	requirements={"domain"="%dash_domain%"}
+     * )
+     */
+    public function indexDash()
+    {
+        return $this->render('frontend/index.html.twig', [
+            'controller_name' => 'indexDash',
         ]);
     }
 }
