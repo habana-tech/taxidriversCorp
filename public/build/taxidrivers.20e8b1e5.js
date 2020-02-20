73e82f0e2dd4779451b5656c7ee8fbd86c413398
Josue Miguel <josmiguel92@gmail.com>
2020-02-20 00:30:50 -0500

    adding build styles

---
+++ public/build/taxidrivers.20e8b1e5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["taxidrivers"],{R6qW:function(n,o,e){e("r9pJ"),console.log("Hello Webpack Encore! Edit me in assets/js/app.js")},r9pJ:function(n,o,e){}},[["R6qW","runtime"]]]);
