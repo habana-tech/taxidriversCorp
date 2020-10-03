a8918aaa64f29d3f6ac5dac73018d94bbb90a138
Josue Miguel <josmiguel92@gmail.com>
2020-10-03 18:18:01 -0400

    update npm & composer dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,23 @@ module.exports = {
             './templates/**/*.html',
             './templates/**/*.twig']
     },
-  theme: {
-      screens: {
-          sm: '640px',
-          md: '768px',
-          lg: '1024px',
-          xl: '1280px',
-      },
-      fontFamily: {
-          display: ['Montserrat', 'sans-serif'],
-          body: ['Hind Madurai', 'sans-serif'],
-      },
-      extend: {},
-  },
-  variants: {},
-  plugins: [],
+    theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+        },
+        fontFamily: {
+            display: ['Montserrat', 'sans-serif'],
+            body: ['Hind Madurai', 'sans-serif'],
+        },
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
 }
